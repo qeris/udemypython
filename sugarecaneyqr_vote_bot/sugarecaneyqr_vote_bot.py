54f81e7c9948806a1d6ef7faa9ca5e74a3b49aaa
Qeris <ethanschoultz@gmail.com>
2024-03-21 13:57:17 -0600

    updated

--- sugarecaneyqr_vote_bot/sugarecaneyqr_vote_bot.py
+++ sugarecaneyqr_vote_bot/sugarecaneyqr_vote_bot.py
@@ -10,11 +10,9 @@ COUNT = 5
 def nominate():
     nominate_button = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div[5]/table/tbody/tr[34]/td[1]/a")
     nominate_button.click()
-    time.sleep(COUNT)
     driver.quit()
 
 for _ in range(COUNT):
     nominate()
     interval = random.randint(1*3600, 6*3600)
     time.sleep(interval)
-    
