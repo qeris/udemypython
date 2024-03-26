fe37f71228050b4beccd2986ce22f6b757aaeccd
Qeris <ethanschoultz@gmail.com>
2024-03-26 14:32:59 -0600

    Changed loop to run 3 times randomly within 15min

--- sugarecaneyqr_vote_bot/sugarecaneyqr_vote_bot.py
+++ sugarecaneyqr_vote_bot/sugarecaneyqr_vote_bot.py
@@ -2,17 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import random
+
 url = "https://regina.communityvotes.com/2024/02/wellness-hair-and-beauty/hair-removal-and-waxing"
-driver = webdriver.Firefox()
-driver.get(url)
-COUNT = 5
 
 def nominate():
+    driver = webdriver.Firefox()
+    driver.get(url)
     nominate_button = driver.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div[5]/table/tbody/tr[34]/td[1]/a")
     nominate_button.click()
     driver.quit()
 
-for _ in range(COUNT):
-    nominate()
-    interval = random.randint(1*3600, 6*3600)
-    time.sleep(interval)
+while True:
+    COUNT = 3
+    nomination_time = 15 * 60
+    interval = nomination_time / COUNT
+    random_sleep_time = random.uniform(interval * 0.5, interval * 1.5)
+    six_hours = 6*3600
+    for _ in range(COUNT):
+        nominate()
+        time.sleep(random_sleep_time)
+
+    time.sleep(six_hours - nomination_time)
